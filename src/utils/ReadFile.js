22bb6c23d08e455cee5ad5cf59d562c62bb8f25a
seonae-j <wkdtjsdo135@naver.com>
2024-11-09 01:06:11 +0900

    fix(ReadFile): 비동기적으로 동작하여 내부에서 함수가 종료되는 오류 수정

--- src/utils/ReadFile.js
+++ src/utils/ReadFile.js
@@ -1,27 +1,31 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import * as fs from 'fs';
+import * as resolve from 'path';
 
 const readFileObject = (filePath) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      MissionUtils.Console.print('파일을 읽는 중 오류가 발생했습니다.', err);
-      return;
-    }
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        MissionUtils.Console.print('파일을 읽는 중 오류가 발생했습니다.', err);
+        reject(err);
+        return;
+      }
 
-    const lines = data.trim().split(`\n`);
-    const headers = lines[0].split(',');
-    const result = lines.slice(1).map((line) => {
-      const values = line.split(',');
-      const valueObject = {};
+      const lines = data.trim().split(`\n`);
+      const headers = lines[0].split(',');
+      const result = lines.slice(1).map((line) => {
+        const values = line.split(',');
+        const valueObject = {};
 
-      headers.forEach((header, index) => {
-        valueObject[header.trim()] = values[index].trim();
+        headers.forEach((header, index) => {
+          valueObject[header.trim()] = values[index].trim();
+        });
+
+        return valueObject;
       });
 
-      return valueObject;
+      resolve(result);
     });
-
-    return result;
   });
 };
 
