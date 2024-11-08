a88939cfeffc3b78b3c507ae002185790d570f1c
seonae-j <wkdtjsdo135@naver.com>
2024-11-09 01:11:44 +0900

    feat(Products): 재고 관리 데이터를 객체화하여 다루기 쉽도록 모델 생성

---
+++ src/models/Products.js
@@ -0,0 +1,10 @@
+class Products {
+  constructor({ name, price, stock, promotion }) {
+    this.name = name;
+    this.price = parseInt(price, 10);
+    this.stock = parseInt(stock, 10);
+    this.promotion = promotion || null;
+  }
+}
+
+export default Products;
