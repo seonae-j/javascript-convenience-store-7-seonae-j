764b1dc88f6b1a739831b9007f2c8a8c56a890c4
seonae-j <wkdtjsdo135@naver.com>
2024-11-11 21:20:16 +0900

    refactor(App): 출력값 확인을 위한 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from '@woowacourse/mission-utils';
+import Purchase from './domains/Purchase.js';
+import Product from './domains/Product.js';
+
 class App {
-  async run() {}
+  async run() {
+    const purchase = new Purchase();
+    const product = new Product();
+
+    try {
+      await product.setProduct();
+      Console.print(product.printProduct());
+      const userInput = await purchase.getPurchase();
+      product.findProduct(userInput.name);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default App;
