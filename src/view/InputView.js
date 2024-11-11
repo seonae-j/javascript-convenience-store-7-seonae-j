561b9efa76c9e8e646868db9bb807a3bf651d4d6
seonae-j <wkdtjsdo135@naver.com>
2024-11-11 21:14:24 +0900

    style(InputView): 입력값 출력할 때 줄바꿈 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,7 +3,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 const InputView = {
   async readItem() {
     const inputPurchase = await MissionUtils.Console.readLineAsync(
-      '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])'
+      '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n'
     );
     return inputPurchase;
   },
